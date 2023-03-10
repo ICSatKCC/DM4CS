56dbc39449acf2f65f5fdab67532e3cc830bca29
Steven Clontz <steven.clontz@gmail.com>
2023-03-10 18:56:18 +0000

    revert &gt; and &lt; in code

--- source/sage_venn-compliment2.py
+++ source/sage_venn-compliment2.py
@@ -2,7 +2,7 @@ scale = 1.7
 sx=3
 sy=2
 G = Graphics()
-r=lambda x,y: x^2+y^2 &gt; scale^2
+r=lambda x,y: x^2+y^2 > scale^2
 G+=region_plot(r,(-sx,sx),(-sy,sy),plot_points=200,incol='lightblue',axes=False)
 G += circle([0,0], scale)
 G+=line([[-sx,-sy],[sx,-sy],[sx,sy],[-sx,sy],[-sx,-sy]],rgbcolor=(0,0,0))
